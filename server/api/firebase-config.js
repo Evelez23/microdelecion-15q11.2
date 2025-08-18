d5b38f0c3d13078c0833779e1b3855a76b8957b9
Evelez23 <elmer_velez@hotmail.com>
2025-08-18 09:55:24 -0600

    Delete server directory

--- server/api/firebase-config.js
+++
@@ -1,5 +0,0 @@
-export const getPatients = async () => {
-  const db = getFirestore();
-  const patientsSnapshot = await db.collection('pacientes').get();
-  return patientsSnapshot.docs.map(doc => doc.data());
-};
