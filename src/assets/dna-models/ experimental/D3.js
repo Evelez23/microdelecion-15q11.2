d7b8bd585530b66d80f4c1c514df8cb9455319fb
Evelez23 <elmer_velez@hotmail.com>
2025-08-21 08:16:55 -0600

    Delete src directory

--- src/assets/dna-models/ experimental/D3.js
+++
@@ -1,3 +0,0 @@
-d3.json('assets/dna-models/experimental/wild-vs-mutant.json').then(data => {
-  // Crear SVG comparativo
-});
--- src/assets/dna-models/ experimental/NIPA1.js
+++
@@ -1,9 +0,0 @@
-const nipa1Models = jsonData.filter(item => item.gene === "NIPA1");
-
-// Datos estructurados para visualización
-const modelsForViz = nipa1Models.map(model => ({
-  id: model.modelEntityId,
-  score: parseFloat(model.globalMetricValue),
-  sequence: model.sequence,
-  organism: model.organismScientificName
-}));
