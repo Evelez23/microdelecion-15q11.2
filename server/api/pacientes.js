3e19760a6889dde3892f74c6d673f2087f35abb2
Evelez23 <elmer_velez@hotmail.com>
2025-08-21 08:17:04 -0600

    Delete server directory

--- server/api/pacientes.js
+++
@@ -1,9 +0,0 @@
-import { getFirestore } from 'firebase-admin/firestore';
-
-export const getPatientsAPI = async (req, res) => {
-  const db = getFirestore();
-  const snapshot = await db.collection('pacientes').get();
-  const patients = snapshot.docs.map(doc => doc.data());
-  
-  res.json(patients);
-};
